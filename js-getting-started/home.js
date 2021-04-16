622e5ace56bf18a7b21dc88e633573b3302c8b13
homedes <cesar.mso@gmail.com>
2021-04-16 13:00:48 +0200

    playing with variables

--- js-getting-started/home.js
+++ js-getting-started/home.js
@@ -5,3 +5,11 @@ showMessage("Changes appear!!");
 showMessage("What now?");
 
 console.log('TEEEEST');
+
+//declaring multiple variables
+let price = 49.99,
+    itemName = 'Some merch',
+    discount = '15%';
+
+//use them all in function
+showMessage(itemName, discount, price);
